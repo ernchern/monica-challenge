cded8f758e1ac35b725ef3d0a9ada557e20550dd
ernchern <39647817+ernchern@users.noreply.github.com>
2020-05-02 21:52:52 +0900

    new ques

--- gibberish.js
+++ gibberish.js
@@ -19,17 +19,24 @@ var daejeon=[
 var bandung=[
 {
 	"city": "bandung",
-	"author": "Alvin",
+	"author": "Coach",
 	"question": "Cue cheese angry erm",
 	"answer": "Cookies and cream",
 	"hint": "It's a type of flavour!"
 },
 {
 	"city": "bandung",
-	"author": "Alvin",
+	"author": "알빈",
 	"question": "Eye ammo knicker",
 	"answer": "I am Monica",
 	"hint": "Who are you?"
+},
+{
+	"city": "bandung",
+	"author": "Ewwinnnn",
+	"question": "Hanzo 파",
+	"answer": "Hans oppa",
+	"hint": "♥️"
 }
 ];
 
